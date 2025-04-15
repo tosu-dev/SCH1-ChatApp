add67c6955659d09f52253bd0a3ba3b931516e34
tosu! <theo.constant42@gmail.com>
2025-04-15 08:33:10 +0200

    Project Setup

---
+++ AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(SCH1ChatApp.Core), "ChatApp", "1.0.0", "L9 Tom&Jerry", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
---
+++ Core.cs
@@ -0,0 +1,31 @@
+﻿using MelonLoader;
+
+namespace SCH1ChatApp;
+
+public class Core : MelonMod
+{
+    private int count = 1;
+    
+    public override void OnInitializeMelon()
+    {
+        LoggerInstance.Msg("Initialized.");
+    }
+
+
+    public override void OnEarlyInitializeMelon()
+    {
+        HelloWorld();
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        Melon<Core>.Logger.Msg("COUNTER : " + count);
+        count++;
+    }
+
+    public static void HelloWorld()
+    {
+        Melon<Core>.Logger.Msg("Hello World from a static method!");
+    }
+}
